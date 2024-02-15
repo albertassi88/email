a96aba08645b00f107d1e55343c4f820f69551b4
Ruben Albertassi <ruben.albertassi@enacom.com.br>
2024-02-15 10:14:55 -0300

    alteração na cor da mensagem de e-mail enviado

--- back/src/api/user/utils/message/index.js
+++ back/src/api/user/utils/message/index.js
@@ -11,7 +11,7 @@ const amanha = (item, element, user) => `
     <h3>Lembrete amigável: o vencimento do seu boleto é <strong>Amanhã<strong>. Pague a tempo e evite taxas extras.</h3>
     <h3>Estamos lhe encaminhando o boleto bancário da ${user.company} com vencimento em ${element.vencimento}.</h3>
     <h3>Qualquer dúvida favor entrar em contato com o financeiro da ${user.company} pelo e-mail ${user.email}.</h3>
-    <h3><strong>Se você já efetuou o pagamento, por favor desconsidere este e-mail.<strong></h3>
+    <h3 style="color: red"><strong>Se você já efetuou o pagamento, por favor desconsidere este e-mail.<strong></h3>
     <h3>Att. ${user.name}.</h3>`
 ;
 
@@ -20,7 +20,7 @@ const hoje = (item, element, user) => `
     <h3>Lembrete amigável: o vencimento do seu boleto é <strong>Hoje<strong>. Pague a tempo e evite taxas extras.</h3>
     <h3>Estamos lhe encaminhando o boleto bancário da ${user.company} com vencimento em ${element.vencimento}.</h3>
     <h3>Qualquer dúvida favor entrar em contato com o financeiro da ${user.company} pelo e-mail ${user.email}.</h3>
-    <h3><strong>Se você já efetuou o pagamento, por favor desconsidere este e-mail.<strong></h3>
+    <h3 style="color: red"><strong>Se você já efetuou o pagamento, por favor desconsidere este e-mail.<strong></h3>
     <h3>Att. ${user.name}.</h3>`
 ;
 
